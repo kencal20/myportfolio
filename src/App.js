987496966b5c76380205e02ec5417686a75efdd9
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-01-07 19:49:09 +0000

    saving current theme so that after refresh the theme dient change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 import Router from './components/Router/router';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
-export const ThemeContext = createContext(null)
+export const ThemeContext = createContext(null);
 
 export default function App() {
-  const [theme, setTheme] = useState('dark')
+  // Retrieve the theme from localStorage or use 'dark' as the default
+  const storedTheme = localStorage.getItem('theme') || 'dark';
+  const [theme, setTheme] = useState(storedTheme);
+
   const toggleTheme = () => {
-    if (theme === 'light') {
-      setTheme('dark')
-    } else {
-      setTheme('light')
-    }
-  }
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    // Save the new theme to localStorage
+    localStorage.setItem('theme', newTheme);
+  };
+
+  // Use useEffect to set the theme class on initial render
+  useEffect(() => {
+    document.getElementById('root').className = theme;
+  }, [theme]);
+
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="App" id={theme}>
         <Router />
       </div>
-    </ThemeContext.Provider >
+    </ThemeContext.Provider>
   );
-};
+}
