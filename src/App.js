7d67d81f9e9b1eab297018596eadc11341645120
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-01-07 19:35:18 +0000

    creating switch buttin for dark and light theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { useState, createContext } from 'react';
 import Router from './components/Router/router';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+export const ThemeContext = createContext(null)
 
 export default function App() {
+  const [theme, setTheme] = useState('dark')
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }
   return (
-
-      <Router/>
-  
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <Router />
+      </div>
+    </ThemeContext.Provider >
   );
-}
-
+};
