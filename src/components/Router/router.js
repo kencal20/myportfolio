c9292da3d6d2fc5d78278fb349ddb76bc1661d51
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-01-07 19:29:41 +0000

    Adding the remaining screens

--- src/components/Router/router.js
+++ src/components/Router/router.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from '../pages/homeScreen'
-import About from '../pages/aboutScreen'
-import Contact from '../pages/contactScreen'
+import Home from '../../pages/homeScreen'
+import About from '../../pages/aboutScreen'
+import Contact from '../../pages/contactScreen'
 import Navbar from '../navbar/navbar'
+import Skills from '../../pages/skillsScreen'
+import Profile from '../../pages/profileScreen'
 
 export default function AppRouter() {
     return (
@@ -13,6 +15,8 @@ export default function AppRouter() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
+                <Route path="/skills" element={<Skills />} />
+                <Route path="/profile" element={<Profile />} />
             </Routes>
         </Router>
     )
