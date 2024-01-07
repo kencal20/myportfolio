91cdab2c6e0008712497509386d61aa437e84823
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-01-07 21:29:49 +0000

    skills screen update

--- src/pages/skillsScreen.jsx
+++ src/pages/skillsScreen.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import  '../css/skills.css'
 
-export default function SkillsScreen() {
+export default function SkillsComponent() {
   return (
-    <div>skillsScreen</div>
-  )
-}
+    <div className="skills-container container">
+      <h2>Skills</h2>
+      <ul className="skills-list">
+        <li>React</li>
+        <li>Node.js</li>
+        <li>JavaScript</li>
+        <li>HTML/CSS</li>
+        <li>React Native/Expo</li>
+      </ul>
+    </div>
+  );
+};
+
+
